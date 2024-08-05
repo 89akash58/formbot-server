f0f7db580fedc7cebb749c5161235fa57e81e233
akash.ssingh.aakkaa@gmail.com <akash.ssingh.aakkaa@gmail.com>
2024-08-05 20:03:07 +0530

    comm

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 });
 app.use("/", userroute);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
